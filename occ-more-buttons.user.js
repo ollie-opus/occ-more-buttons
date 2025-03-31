1b982cf04362af0cd268773e60822c9d2daee2b4
ollie-opus <ollie@opus-safety.co.uk>
2025-03-31 11:44:15 +0100

    Update and rename occ-more-buttons.js to occ-more-buttons.user.js

--- occ-more-buttons.user.js
+++ occ-more-buttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         occ-more-buttons
 // @namespace    http://tampermonkey.net/
-// @version      1.0.8
+// @version      1.0.9
 // @description  Adds more Buttons. So far: Set as manager, Set as user, Clear checkboxes and Generate QR poster.
 // @author       Ollie
 // @match        https://cloud.opus-safety.co.uk/*
