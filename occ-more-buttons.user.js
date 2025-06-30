cf16af5ee6271f4c5cefc54918036da23a31d8f8
ollie-opus <olliepotter1@gmail.com>
2025-06-30 16:50:19 +0100

    Update occ-more-buttons.user.js
    Added Audit CSV Export Button

--- occ-more-buttons.user.js
+++ occ-more-buttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         occ-more-buttons
 // @namespace    http://tampermonkey.net/
-// @version      1.1.8
+// @version      1.1.9
 // @description  Adds more Buttons. So far: Set as manager, Set as user, Clear checkboxes and Generate QR poster.
 // @author       Ollie
 // @match        https://cloud.opus-safety.co.uk/*
@@ -23,6 +23,53 @@
     function clearAllCheckboxes() {
         const checkboxes = document.querySelectorAll('input[type="checkbox"]');
         checkboxes.forEach(checkbox => checkbox.checked = false);
+    }
+
+    function auditCSVExport() {
+
+        // Select all <li> elements with both specific data attributes
+        const questionBlocks = document.querySelectorAll('li[data-collapse-target="content"][data-reorderable-group="question"]');
+
+        // Define CSV header
+        let csvRows = [
+            ['Section', 'Question', 'Weighting', 'Question Type', 'Audit Action Severity', 'Audit Action Title']
+        ];
+
+        questionBlocks.forEach(block => {
+            const section_number = block.querySelector('input[name$="[section]"]')?.value || '';
+            const section = document.getElementById(`section-${section_number}-name`)?.value || '';
+            const question = block.querySelector('input[name$="[title]"]')?.value || '';
+            const weighting = block.querySelector('input[name$="[weight]"]')?.value || '';
+            const question_type = block.querySelector('input[name$="[type]"]')?.value || '';
+            const audit_action_severity = block.querySelector('select[name$="[todo_severity]"]')?.value || '';
+            const audit_action_title = block.querySelector('input[name$="[todo_title]"]')?.value || '';
+
+            const row = [section, question, weighting, question_type, audit_action_severity, audit_action_title];
+
+            // Audit Header (Other Information) exclusion rule
+            if (section === "Other information") {
+                return
+            }
+
+            // Only include the row if at least one field is not empty
+            if (row.some(cell => cell.trim() !== '')) {
+                csvRows.push(row);
+            }
+        });
+
+        // Convert rows to CSV string
+        const csvContent = csvRows
+        .map(row => row.map(cell =>`"${cell.replace(/\r?\n|\r/g, ' ').replace(/"/g, '""')}"`).join(',')).join('\n');
+
+
+        const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+        const link = document.createElement('a');
+        const rawTitle = document.getElementById('page-title')?.textContent.trim() || '';
+        const cleanedTitle = rawTitle.replace(/^Edit\s+/i, '');
+        link.href = URL.createObjectURL(blob);
+        link.download = `${cleanedTitle} data-export.csv`;
+        link.click();
+
     }
 
     function annualDocumentDefault() {
@@ -513,7 +560,7 @@
         document.body.appendChild(button);
     }
 
-        function addAnnualDocumentDefaultButton() {
+    function addAnnualDocumentDefaultButton() {
         if (document.getElementById('annualDocumentDefault')) return;
 
         let button = document.createElement('button');
@@ -624,12 +671,51 @@
         document.body.appendChild(button);
     }
 
+    function addAuditCSVExportButton() {
+        if (document.getElementById('auditCSVExport')) return;
+
+        let button = document.createElement('button');
+        button.id = 'auditCSVExport';
+        button.innerText = 'Audit CSV export';
+        button.title = 'Export this audit to CSV';
+        button.style.position = 'fixed';
+        button.style.top = '10px';
+        button.style.right = '210px';
+        button.style.padding = '9px 13px';
+        button.style.backgroundColor = '#047857';
+        button.style.color = 'white';
+        button.style.border = 'none';
+        button.style.borderRadius = '3px';
+        button.style.cursor = 'pointer';
+        button.style.zIndex = '1000';
+        button.style.fontWeight = '500';
+        button.style.fontFamily = 'RubikVariable, ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji';
+        button.style.fontSize = '.875rem';
+
+        button.addEventListener('click', function() {
+            auditCSVExport();
+        });
+
+        document.body.appendChild(button);
+    }
+
     // 3rd ORDER FUNCTIONS
 
     function handleEditPageFilter() {
         if (window.location.href.includes("/edit")) {
             setTimeout(() => {
                 addCheckboxButton(); // Trigger addButton
+                // add more triggers if needed
+            }, 1000);
+        } else {
+            return; // If URL doesn't match, stop the script
+        }
+    }
+
+    function handleAuditEditPageFilter() {
+        if (window.location.href.includes("templates/checklists/dGF") && window.location.href.includes("/edit?checklist=")) {
+            setTimeout(() => {
+                addAuditCSVExportButton(); // Trigger addButton
                 // add more triggers if needed
             }, 1000);
         } else {
@@ -771,6 +857,7 @@
             handleMPPFilters();
             handleChecklistTemplatePageFilter();
             handleNewDocumentPageFilter();
+            handleAuditEditPageFilter();
         }
 
         // Intercept pushState and replaceState for SPA URL changes
